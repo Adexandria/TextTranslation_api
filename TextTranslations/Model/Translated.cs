504ceb08a183710092ea5429452a04270bee823e
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-11-13 13:09:51 +0100

    fixed syntax error

--- TextTranslations/Model/Translated.cs
+++ TextTranslations/Model/Translated.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ namespace TextTranslations.Model
 {
     public class Translated
     { 
-        []
+        [JsonProperty("translations")]
         public IList<Translations> translations { get; set; }
     }
 }
--- TextTranslations/Model/Translations.cs
+++ TextTranslations/Model/Translations.cs
@@ -1,6 +1,13 @@
-﻿namespace TextTranslations.Model
+﻿using Newtonsoft.Json;
+
+namespace TextTranslations.Model
 {
     public class Translations
     {
+        [JsonProperty("text")]
+        public string text { get; set; }
+
+        [JsonProperty("to")]
+        public string to { get; set; }
     }
 }
