9d2fde727537b2df3bdf34e62096b24fb3e68118
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2021-04-02 08:13:08 +0100

    included text to speech api

--- TextTranslations/Controllers/TranslateText.cs
+++ TextTranslations/Controllers/TranslateText.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -22,36 +23,55 @@ namespace TextTranslations.Controllers
     {
         static string subscriptionKey = Environment.GetEnvironmentVariable("COMPUTER_VISION_SUBSCRIPTION_KEY");
         static string endpoint = Environment.GetEnvironmentVariable("COMPUTER_VISION_ENDPOINT");
-         string uriBase = endpoint + "vision/v3.0/ocr";
+        string uriBase = endpoint + "vision/v3.0/ocr";
 
         static string subscriptionKey1 = Environment.GetEnvironmentVariable("TRANSLATOR_SUBSCRIPTION_KEY");
         static string endpoint1 = Environment.GetEnvironmentVariable("TRANSLATOR_ENDPOINT");
         static string location = Environment.GetEnvironmentVariable("LOCATION");
 
+        static string speechKey = Environment.GetEnvironmentVariable("SPEECH_TRANSLATOR_SUBSCRIPTION_KEY");
+        static string speechEndpoint = Environment.GetEnvironmentVariable("SPEECH_TRANSLATOR_KEY");
+        static string firstpath = Path.GetFullPath("C:\\Users\\HP 650\\source\\repos\\TextTranslations\\Speaker.ssml");
+        static string path = Path.GetFullPath("C:\\Users\\HP 650\\source\\repos\\TextTranslations\\Speakers.ssml");
         string uriBase1 = endpoint1+ "translate";
 
         [HttpPost]
         public async Task<ActionResult<JsonProperty>> Post(IFormFile file,string lang)
         {
-            if (file == null)
+            try
             {
+                if (lang == null)
+                {
+                    return BadRequest("language not requested");
+                }
+                if (file == null)
+                {
                 return this.StatusCode(StatusCodes.Status404NotFound, "file not found");
-            }
-           string details = await MakeAnalysisRequest(file);
-            Analyze analyze = JsonConvert.DeserializeObject<Analyze>(details);
-            var words = analyze.regions.SelectMany(e => e.lines.SelectMany(e => e.words).Select(s => s.text)).ToList();
-            string sentence = String.Join(" ", words);
-            Texts text = new Texts
-            {
+                }
+                string details = await MakeAnalysisRequest(file);
+                Analyze analyze = JsonConvert.DeserializeObject<Analyze>(details);
+                var words = analyze.regions.SelectMany(e => e.lines.SelectMany(e => e.words).Select(s => s.text)).ToList();
+                string sentence = String.Join(" ", words);
+                Texts text = new Texts
+                {
                 Text = sentence
-            };
-            List<Texts> texts = new List<Texts>
-            {
+                };
+                List<Texts> texts = new List<Texts>
+                {
                 text
-            };
-            var newlang = await Translate(texts, lang);
-            List<Translated> translations = JsonConvert.DeserializeObject<List<Translated>>(newlang);
-            return Ok(translations.SelectMany(r=>r.translations.Select(r=>r.text)));
+                };
+                var newlang = await Translate(texts, lang);
+                List<Translated> translations = JsonConvert.DeserializeObject<List<Translated>>(newlang);
+                var speech = await TexttoSpeech(translations.SelectMany(r => r.translations.Select(r => r.text)));
+ 
+                return Ok(speech);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+          
         }
 
         [NonAction]
@@ -60,8 +80,7 @@ namespace TextTranslations.Controllers
 
             try
             {
-                HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                var client = GetClient(subscriptionKey);
                 string uri = uriBase;
                 HttpResponseMessage httpResponse;
                 byte[] bytedata = GetImageAsByteArray(image.FileName);
@@ -78,8 +97,8 @@ namespace TextTranslations.Controllers
             }
             catch (Exception e)
             {
-
-                return e.Message;
+                string message = "Language format not accepted";
+                return message;
             }
         }
         [NonAction]
@@ -88,8 +107,7 @@ namespace TextTranslations.Controllers
 
             try
             {
-                HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey1);
+                var client = GetClient(subscriptionKey1);
                 client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Region",location);
                 var requestparameter = "api-version=3.0&to=" + lang;
                 string uri = uriBase1+"?"+requestparameter;
@@ -112,14 +130,65 @@ namespace TextTranslations.Controllers
                 return e.Message;
             }
         }
-        private byte[] GetImageAsByteArray(string fileName)
+   
+        private async Task<string> TexttoSpeech(IEnumerable<string> text)
         {
-            using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            try
             {
-                BinaryReader binary = new BinaryReader(fileStream);
-                return binary.ReadBytes((int)fileStream.Length);
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", speechKey);
+                client.DefaultRequestHeaders.Add("X-Microsoft-OutputFormat", "raw-24khz-16bit-mono-pcm");
+                client.DefaultRequestHeaders.Add("User-Agent", "TranslateSpeech");
+                HttpResponseMessage httpResponse;
+                var singleString = string.Join("", text.ToArray());
+                ReadandWriteText(singleString);
+                var str = new FileStream(path,FileMode.Open);
+                using (StreamContent content = new StreamContent(str))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/ssml+xml");
+                    httpResponse = await client.PostAsync(speechEndpoint, content);
+                }
+                var contentStream = await httpResponse.Content.ReadAsStringAsync();
+                var newContent = JToken.Parse(contentStream).ToString();
+                return newContent;
 
             }
+            catch (Exception e)
+            {
+
+                return e.Message;
+            }
+        }
+        private HttpClient GetClient(string key) 
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+            return client;
+        }
+        private byte[] GetImageAsByteArray(string fileName)
+        {
+            using FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader binary = new BinaryReader(fileStream);
+            return binary.ReadBytes((int)fileStream.Length);
+        }
+        private void ReadandWriteText(string text)
+        {
+           var st = System.IO.File.ReadAllLines(firstpath);
+            StringBuilder s = new StringBuilder();
+            for(int i = 0; i < st.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(st[i])) 
+                {
+                    s.Append(text);
+                }
+                else 
+                {
+                    s.Append(st[i]);
+                }     
+            }
+            var str = new StreamWriter(path);
+            str.Write(s.ToString());
+            str.Close();   
         }
        
     }
